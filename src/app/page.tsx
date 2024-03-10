204f248c1481fe40a3e5d8a4e9cf95ded08babe6
Dario Reis <guttyerresjunior1995@gmail.com>
2024-03-09 22:11:54 -0300

    feat: data fetch in components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
-export default function Home() {
-  return <h1>Hello world</h1>
+export default async function Home() {
+  await new Promise((resolve) => setTimeout(resolve, 2000)) // Simulate loading time.
+
+  const response = await fetch('https://api.github.com/users/darioreisjr')
+  const user = await response.json()
+
+  return <pre>{JSON.stringify(user, null, 2)}</pre>
 }
