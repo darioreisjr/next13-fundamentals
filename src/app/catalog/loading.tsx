7bc06de46c3f5d6b41113233b55756769c3003ec
Dario Reis <guttyerresjunior1995@gmail.com>
2024-03-10 21:05:51 -0300

    feat: SSR Loading and Streaming

---
+++ src/app/catalog/loading.tsx
@@ -0,0 +1,3 @@
+export default function CatalogLoading() {
+  return <h1>Carregando catalogo...</h1>
+}
--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,3 +1,5 @@
-export default function Catalog() {
+export default async function Catalog() {
+  await new Promise((resolve) => setTimeout(resolve, 2000)) // Simulate loading time.
+
   return <h1>Catalog</h1>
 }
